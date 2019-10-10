489c27e5085b10feb36751fc7988b8795c1cdf81
Hlamalani MKABELA <hmkabela@c4r11s11.wethinkcode.co.za>
2019-10-10 18:35:14 +0200

    .

--- d01/ex01/mlx.php
+++ d01/ex01/mlx.php
@@ -4,17 +4,14 @@
 	$num2 = 0;
 	while($num1 != 1000)
 	{
-		if($num2 == 100)
+		if($num2 == 99)
 		{
 			echo "X\n";
-			$num2 = 0;
+			$num2 = -1;
 		}
 		else
-		{
 			echo "X";
-			$num1++;
-			$num2++;
-		}
+		$num1++;
+		$num2++;
 	}
-	echo "X\n";
 ?>
--- d01/ex03/main.php
+++ d01/ex03/main.php
@@ -1,5 +1,5 @@
 #!/usr/bin/php
 <?php
 	include("ft_split.php");
-	print_r(ft_split("  dawg    Hello    World       AAA       "));
+	print_r(ft_split("  dawg 1   Hello    World       AAA       "));
 ?>
